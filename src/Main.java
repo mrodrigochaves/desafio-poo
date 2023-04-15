282da0e85cac2b83670f4bd43697d7a4aae09433
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-04-15 17:27:23 -0300

    Add curso 1 e 2

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,18 @@ public class Main {
     public static void main(String[] args) {
         
         Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Javascript");
+        curso2.setDescricao("descrição curso javascript");
+        curso2.setCargaHoraria(4);
         
 
         System.out.println(curso1);
+        System.out.println(curso2);
     }
 }
